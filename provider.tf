dd3b98f2ad793ed28de88626cc26dca6267fb2f9
Tobi <148586163+iam-fawwy@users.noreply.github.com>
2024-01-23 22:21:55 +0000

    output added

--- provider.tf
+++ provider.tf
@@ -2,10 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
+      version = "4.2.0"
     }
   }
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region  = "eu-west-2"
 }
